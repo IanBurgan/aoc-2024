8a003beab33ccff328d1dcb69b3fbefeb24ab559
Ian Burgan <burganian@gmail.com>
2024-12-03 00:19:46 -0500

    Clean day 2

--- 2.py
+++ 2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-def is_valid(nums):
+def is_safe(nums):
     is_inc = nums[1] > nums[0]
 
     for i in range(1, len(nums)):
         if (nums[i] > nums[i - 1]) != is_inc:
             return False
-        if abs(nums[i] - nums[i - 1]) > 3 or abs(nums[i] - nums[i - 1]) < 1:
+        diff = abs(nums[i] - nums[i -1])
+        if not (1 <= diff <= 3):
             return False
 
     return True
@@ -22,14 +23,14 @@ part_two = 0
 for l in lines:
     nums = [int(x) for x in l.strip().split()]
 
-    if is_valid(nums):
+    if is_safe(nums):
         part_one += 1
         part_two += 1
     else:
         for i in range(len(nums)):
             new_nums = nums.copy()
             del new_nums[i]
-            if is_valid(new_nums):
+            if is_safe(new_nums):
                 part_two += 1
                 break
 
