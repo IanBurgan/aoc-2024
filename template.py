b001b6a1cedaeab73773b1601411727d09a26833
Ian Burgan <burganian@gmail.com>
2024-12-04 02:17:39 -0500

    Update template file, add defaultdict grid

--- template.py
+++ template.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
-myfile = open('X.in', 'r')
+from collections import defaultdict
+
+myfile = open("X.in", "r")
 lines = myfile.read().strip().splitlines()
 myfile.close()
 
-part_one = ''
-part_two = ''
+grid = defaultdict(str)
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        grid[(x,y)] = lines[y][x]
 
-for l in lines:
-    l = l.strip()
+part_one = 0
+part_two = 0
 
-print('Part One:', part_one)
-print('Part Two:', part_two)
+for line in lines:
+    line = line.strip()
+
+print("Part One:", part_one)
+print("Part Two:", part_two)
