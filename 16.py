0192b6ea28e7c2605d633708489a2584804a5785
Ian Burgan <burganian@gmail.com>
2024-12-16 05:23:45 -0500

    Clean up day 16

--- 16.py
+++ 16.py
@@ -22,35 +22,33 @@ dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 part_one = 0
 part_two = 0
 
+scores = defaultdict(lambda: float("inf"))
 prev = defaultdict(set)
-scores = {}
 # score, pos, direction
 q = [(0, start, 0)]
 heapq.heapify(q)
 while q:
     curr_score, curr_pos, curr_dir = heapq.heappop(q)
+    if grid[curr_pos] == "E":
+        if part_one == 0:
+            part_one = curr_score
+
+        if curr_score > part_one:
+            break
 
     for next_dir in range(len(dirs)):
         next_pos = (curr_pos[0] + dirs[next_dir][0], curr_pos[1] + dirs[next_dir][1])
         next_score = curr_score + 1 if curr_dir == next_dir else curr_score + 1001
 
-        if (next_pos, next_dir) in scores:
-            if next_score > scores[(next_pos, next_dir)]:
-                continue
-            if next_score < scores[(next_pos, next_dir)]:
+        if next_score < scores[(next_pos, next_dir)]:
+            if grid[next_pos] != "#":
+                heapq.heappush(q, (next_score, next_pos, next_dir))
+                scores[(next_pos, next_dir)] = next_score
                 prev[(next_pos, next_dir)].clear()
-
-        scores[(next_pos, next_dir)] = next_score
-        prev[(next_pos, next_dir)].add((curr_pos, curr_dir))
-
-        if grid[next_pos] == ".":
-            heapq.heappush(q, (next_score, next_pos, next_dir))
-        elif grid[next_pos] == "E":
-            if part_one == 0:
-                part_one = next_score
-
-            if next_score > part_one:
-                q.clear()
+                prev[(next_pos, next_dir)].add((curr_pos, curr_dir))
+        elif next_score == scores[(next_pos, next_dir)]:
+            scores[(next_pos, next_dir)] = next_score
+            prev[(next_pos, next_dir)].add((curr_pos, curr_dir))
 
 path_nodes = set()
 remaining = deque([(end, i) for i in range(len(dirs))])
